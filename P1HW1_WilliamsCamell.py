788d6dcb2f4d791acc0bd977765fd3e6f1f2b9c6
Williamc6901 <55545711+Williamc6901@users.noreply.github.com>
2019-09-19 10:31:44 -0400

    Add files via upload
    First submission

---
+++ P1HW1_WilliamsCamell.py
@@ -0,0 +1,10 @@
+# This program demonstrates how the range
+# function can be used with a for loop.
+
+def main():
+    # Print a message five times.
+    For x in range(5):
+        print('Hello world!')
+
+# Call the main function.
+main()
